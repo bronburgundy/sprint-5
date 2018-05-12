47440e6a2caff475ccf5c54078196c918b4d4720
bronburgundy <bron.cotter@icloud.com>
2018-05-12 14:00:43 +1200

    JS Programe Structure

---
+++ eloquent-2.js
@@ -0,0 +1,23 @@
+//LOOPING A TRIANGLE
+
+for(let hash = "#"; hash.length <= 7; hash += "#") {
+	console.log(hash); 
+}
+
+//FIZZBUZZ
+
+for(number = 1; number <=100; number++) {
+  if(number % 3 == 0 && number % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if(number % 3 == 0) {
+    console.log("Fizz");
+  } else if(number % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(number);
+  }
+}
+
+//CHESSBOARD
+
+//Didn't get this one at all!
